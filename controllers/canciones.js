f7a2ffd869ffc2994c670e8c1c5e22c0ee45d5d0
Ricardo Mélida <ricardomelida92@gmail.com>
2020-05-25 21:59:45 -0400

    Se crea carpeta por cada autor de audio subido
    Se logra poder crear carpetas fuera de la carpeta del proyecto
    Solo faltario poder guardar los datos a mongo.

--- controllers/canciones.js
+++ controllers/canciones.js
@@ -1,19 +1,49 @@
 'use strict';
 const path = require('path');
+const fs = require('fs');
+const Cancion = require('../models/canciones');
 
 let canciones = {
     agregar: (req, res) => {
         let datos = req.body;
         let cancion = req.files.audio;
-
-        //console.log(path.join(__dirname, 'files', cancion.name));
-        cancion.mv(path.join(__dirname, 'files', cancion.name), err =>{
+        let ruta = `../canciones/${datos.autor}/`;
+        
+        // Vemos si no existe la carpeta del artista donde guardaremos la cancion
+        if(!fs.existsSync(path.resolve(ruta))) {
+          fs.mkdirSync(path.resolve(ruta), (err)=>{
+            console.log(err);
+          });
+        }
+        // Movemos el audio en la carpeta del autor correspondiente
+        cancion.mv(path.resolve(`${ruta}${datos.nombre}`), err =>{
           if(err) {
             return res.status(500).send({ message: err});
           }
 
           return res.status(200).send({ message: 'Archivo Guardado' });
         });
+
+        let audio = new Cancion();
+
+        audio.nombre = datos.nombre;
+        audio.autor = datos.autor;
+        audio.genero = datos.genero;
+        audio.duracion = datos.duracion;
+
+        audio.save((err, musica)=>{
+          if(err || !musica) {
+            return res.status(404).send({
+                status: 'error',
+                message: "El audio no se ha guardado !!"
+            });
+        }else {
+            //Devolver una respuesta
+            return res.status(200).send({
+                status: 'success'
+            });
+        }
+        })
     },
 
     ver: (req, res) => {
