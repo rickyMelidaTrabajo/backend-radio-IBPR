8a1cd7f43de8468a1a9c31280349a31c6256f51e
Ricardo Mélida <ricardomelida92@gmail.com>
2020-06-06 08:19:41 -0400

    Modelo de la coleccion reproductor
    Se agrega y modifica el modelo de como tiene que ser nuestra coleccion
    del reproductor que va a reproducirse en un dia en especifico. Todo
    esto se creó con el esquema de mongoose.

--- models/reproductor.js
+++ models/reproductor.js
@@ -3,12 +3,16 @@ const mongoose = require('mongoose');
 const schema = mongoose.Schema;
 
 let reproductorSchema = schema({
-    pos: Number,
-    nombre: String,
-    artista: String,
-    tipo_audio: String,
-    duracion: String,
-    hora_rep: String
+    fecha: String[
+        {
+            pos: Number,
+            nombre: String,
+            artista: String,
+            tipo_audio: String,
+            duracion: String,
+            hora_rep: String
+        }
+    ]
 });
 
 module.exports = mongoose.model('Reproductor', reproductorSchema);
