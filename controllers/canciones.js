bcc712abb3466aedcbe6703ba115274c5a2c786f
Ricky Melida <ricardomelida92.trabajo@gmail.com>
2020-06-13 14:33:08 -0400

    Se agrega gitignore

--- controllers/canciones.js
+++ controllers/canciones.js
@@ -6,7 +6,6 @@ const audioLoader = require('audio-loader');
 
 
 let canciones = {
-<<<<<<< HEAD
   agregar: (req, res) => {
     let datos = req.body;
     let cancion = req.files.audio;
@@ -62,36 +61,6 @@ let canciones = {
           });
         }
       });
-=======
-    agregar: (req, res) => {
-        let audio = req.files.audio;
-        let nombreAudio = req.body.nombre;
-        let autor = req.body.autor;
-        let genero = req.body.genero;
-
-        //ruta donde se van a guardat todas las canciones
-        let rutaAbsoluta = path.resolve('../canciones');
-        let rutaArtista = path.join(`${rutaAbsoluta}`, `${autor}`);
-
-        if(!fs.existsSync(rutaArtista)){
-          fs.mkdir(rutaArtista, ()=>{
-            console.log('Se crea la carpeta ya que no existia');
-          });
-        }
-        
-        //console.log(rutaArtista);
-        //Movemos la cancion que se sube a la carpeta de acuerdo al autor del audio
-        audio.mv(`${rutaArtista}/${nombreAudio}.mp3`,err =>{
-          if(err) {
-            return res.status(500).send({menaje: err});
-          }
-
-          return res.status(200).send({menaje: 'Se guardo la cancion'})
-        });
-
-        
-    },
->>>>>>> 3a8e2530834ddddaad40ada5d00640bc2e4f8935
 
 
     })
