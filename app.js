8779833c2100e65d40fe9d0c3561418121f7b867
Ricky Melida <ricardomelida92.trabajo@gmail.com>
2020-06-23 10:14:05 -0400

    Se inserta cors
    Se agregan cors en el backend, sin esto no funciona la API

--- app.js
+++ app.js
@@ -5,6 +5,15 @@ const app = express();
 
 app.use(fileUpload());
 
+//Cargar CORS para realizar peticiones desde frontend
+// Configurar cabeceras y cors
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
 let rutasCanciones  = require('./routes/canciones');
 let rutasReproductor = require('./routes/reproductor');
 
